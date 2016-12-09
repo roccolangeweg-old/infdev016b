74e1eb4c5bad288ae4db88ffc73ef69b9370f052
WildTornado <brandon_vink@hotmail.com>
2016-12-09 12:59:09 +0100

    Added simple database migrations.

--- hangman/models.py
+++ hangman/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=20)
+    wins = models.IntegerField(default=0)
+    forfeits = models.IntegerField(default=0)
+    losses = models.IntegerField(default=0)
+
+
+class Words(models.Model):
+    word = models.CharField(max_length=50)
